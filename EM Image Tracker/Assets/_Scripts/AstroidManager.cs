390d44b2cbbdaddc4bf684996810d8c7d1627d73
ccpddp21 <christopher.peele21@gmail.com>
2021-06-16 22:09:34 -0400

    AR Astronaut scene

---
+++ EM Image Tracker/Assets/_Scripts/AstroidManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+    }
+
+    public void StartDisableCooldown()
+    {
+        audioSource.Play();
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+        StartCoroutine(DisableCooldown());
+    }
+
+    private IEnumerator DisableCooldown()
+    {
+        yield return new WaitForSeconds(2);
+
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+}
---
+++ EM Image Tracker/Assets/_Scripts/TouchInteraction.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class TouchInteraction : MonoBehaviour {
+
+    // You can add listeners in inspector
+    public UnityEvent OnSingleTap;
+    public UnityEvent OnDoubleTap;
+
+
+    float firstTapTime = 0f;
+    float timeBetweenTaps = 0.2f; // time between taps to be resolved in double tap
+    bool doubleTapInitialized;
+
+    void OnMouseDown()
+    {
+        if (!doubleTapInitialized)
+        {
+            // invoke single tap after max time between taps
+            Invoke("SingleTap", timeBetweenTaps);
+            // init double tapping
+            doubleTapInitialized = true;
+            firstTapTime = Time.time;
+        }
+        else if (Time.time - firstTapTime < timeBetweenTaps)
+        {
+            // here we have tapped second time before "single tap" has been invoked
+            CancelInvoke("SingleTap"); // cancel "single tap" invoking
+            DoubleTap();
+        }
+    }
+
+    void SingleTap()
+    {
+        doubleTapInitialized = false; // deinit double tap
+
+        // fire OnSingleTap event for all eventual subscribers
+        if(OnSingleTap != null)
+        {
+            OnSingleTap.Invoke();
+        }
+    }
+
+    void DoubleTap()
+    {
+        doubleTapInitialized = false;
+        if(OnDoubleTap != null)
+        {
+            OnDoubleTap.Invoke();
+        }
+    }
+}
